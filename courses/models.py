90309eea879648c94901144b2b0e87e6e8c5857a
satjeet <arturo.alfaro87@gmail.com>
2016-04-10 11:48:36 -0300

    Antes de comenzar la autentificacion

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
+
 
 # Create your models here.
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    owner = models.ForeignKey(User, related_name='courses_created')
+    subject = models.ForeignKey(Subject, related_name='courses')
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
+
+
+class Module(models.Model):
+    course = models.ForeignKey(Course, related_name='modules')
+
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    order =OrderField(blank=True,for_fields=['course'])
+
+    def __str__(self):
+        return '{}. {}' .format(self.order,self.title)
+
+    class Meta:
+        ordering=['order']
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents')
+
+
+    content_type = models.ForeignKey(ContentType, limit_choices_to={'model__in':('text','video','image','file')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering=['order']
+
+# el padre de los modelos para los distintos tipos de pontenido
+class ItemBase(models.Model):
+    owner=models.ForeignKey(User,related_name='%(class)s_related')
+    title=models.CharField(max_length=250)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract=True
+
+    def __str__(self):
+        return self.title
+# text_related,file_related ,etc
+
+class Text(ItemBase):
+    content =models.TextField()
+
+class File(ItemBase):
+    file= models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    file=models.FileField(upload_to='images')
+
+class Video(ItemBase):
+    url=models.URLField()
+
